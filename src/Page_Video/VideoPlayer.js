90db8d13dac80e29444eed96db0ec78973068fba
girard-noel <girardperugo2000@gmail.com>
2023-10-01 04:03:43 +0900

    fafafcccfeed

--- src/Page_Video/VideoPlayer.js
+++ src/Page_Video/VideoPlayer.js
@@ -7,23 +7,35 @@ const VideoPlayer = (props) => {
   let player;
 
   useEffect(() => {
-    // Check if Video.js player has not been initialized already
-    if (videoNode.current && !videoNode.current.player) {
-      player = videojs(videoNode.current, props);
-      player.playbackRate(0.3);
+    try {
+      // Check if Video.js player has not been initialized already
+      if (videoNode.current && !videoNode.current.player) {
+        player = videojs(videoNode.current, props);
+        player.playbackRate(0.3);
+
+        // Log any video.js errors
+        player.on('error', function() {
+          console.error('Video.js Error:', this.error());
+        });
+      }
+    } catch (error) {
+      console.error('Video Initialization Error:', error);
     }
 
     return () => {
       // Dispose of any Video.js player instances on cleanup
-      if (player) {
-        player.dispose();
+      try {
+        if (player) {
+          player.dispose();
+        }
+      } catch (error) {
+        console.error('Video Disposal Error:', error);
       }
     };
   }, [props]);
-  
 
   return (
-<div data-vjs-player style={{ width: "100%",position:"relative",PaddingBottom:"100%",overflow:"hidden"}}>
+    <div data-vjs-player style={{ width: "100%", position: "relative", paddingBottom: "100%", overflow: "hidden" }}>
       <video ref={videoNode} className="video-js" style={{ width: '100%', height: '100%' }} />
     </div>
   );
