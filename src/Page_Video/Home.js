2d76c807e86879e297b1fb245aadc27433383125
girard-noel <girardperugo2000@gmail.com>
2023-10-01 06:29:11 +0900

    fafafcccfeed

--- src/Page_Video/Home.js
+++ src/Page_Video/Home.js
@@ -2,17 +2,17 @@
 import React,{useState,useEffect,useRef} from 'react';
 import { useLocation } from 'react-router-dom';
 import VideoWrapper from './VideoWrapper';
-
+import NewVideoPlayer from './NewVideoPlayer';
 export const Home=()=> {  
 
     const videoSrc="https://pythonmodulefol.s3.ap-northeast-1.amazonaws.com/EgUgb9GQXYjQ/output.m3u8";
     return (
         <div>
           <div>
-          <VideoWrapper src={videoSrc} />
+          <NewVideoPlayer src={videoSrc} />
           </div>
         Video Src: {videoSrc}
       </div>
     )
 }
-//            
+//                      <VideoWrapper src={videoSrc} />
